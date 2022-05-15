c15fcc56e30f0124e40a0df32dbdf74cb76cdc1e
Preshh0 <uniquep201@gmail.com>
2022-05-15 16:32:42 +0100

    Lazerpay API works now.

--- payment/urls.py
+++ payment/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     # path('done/', payment_done, name='done'),
     path('canceled/', payment_canceled, name='canceled'),
     path('apitest', PaymentLink.as_view()),
-    path('confirm', ConfirmPayment.as_view())
+    path('confirm', ConfirmPayment.as_view()),
+    path('get_coins/', GetCoins.as_view()),
+    path('get_info/', GetPaymentInfo.as_view())
 ]
